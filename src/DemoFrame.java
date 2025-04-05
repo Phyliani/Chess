8100e15f39500b391c3ada902892207ea6d4c894
Phyliani <eliasbdm1008@gmail.com>
2025-04-05 20:51:58 +0200

    Grundlayout Übung Start

---
+++ src/DemoFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class DemoFrame extends JFrame{
+
+    public DemoFrame(){
+        setTitle("Layout demo");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ src/Launcher.java
@@ -0,0 +1,8 @@
+public class Launcher {
+
+    public static void main(String[] args){
+    DemoFrame frame = new DemoFrame();
+    frame.setVisible(true);
+    }
+    
+}
